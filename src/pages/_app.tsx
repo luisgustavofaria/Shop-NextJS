0b42fadfae81936b6f0ea003389d7a8a115191a6
Luis <luis.gustavo.fariac@gmail.com>
2024-05-07 12:44:32 -0300

    criando next image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,22 @@
+import { Container, Header } from '@/components/layout/styles';
 import { GlobalStyle } from '@/styles';
+import logo from '../assets/logo.svg';
 
 import theme from '@/styles/theme';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
+import Image from 'next/image';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Component {...pageProps} />;
+      <Container>
+        <Header>
+          <Image src={logo} alt="" width={130} height={52} priority={true} />
+        </Header>
+        <Component {...pageProps} />;
+      </Container>
     </ThemeProvider>
   );
 }
