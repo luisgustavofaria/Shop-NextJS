461c55590b3ad3a873682e84ea429e2142a53581
Luis <luis.gustavo.fariac@gmail.com>
2024-05-08 22:44:45 -0300

    geração de páginas estáticas, e como utilizar o SSG para criar uma versão estática da nossa página que fica cacheada para acelerar o carregamento e até diminuir o número de requisições à API.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 import 'keen-slider/keen-slider.min.css';
 import { useKeenSlider } from 'keen-slider/react';
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import { stripe } from '../lib/stripe';
 import Stripe from 'stripe';
 
@@ -44,7 +44,7 @@ export default function Home({ products }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe?.products.list({
     expand: ['data.default_price'],
   });
@@ -66,5 +66,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       products,
     },
+    revalidate: 60 * 60 * 2, // 2 hours,
   };
 };
